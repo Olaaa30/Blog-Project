70b8374184c5b869caa8da4b83099bf9dc4e08d2
Olayinka Adebisi <adebisiolayinka30@gmail.com>
2022-06-09 10:58:32 +0100

    (olayinka) made new changes

--- mysite/blog/views.py
+++ mysite/blog/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
-
+from blog.models import Post, Comment
+from django.views.generic import (TemplateView, ListView)
 # Create your views here.
+
+class AboutView(TemplateView):
+    template_name = 'about.html'
+
+class PostListView()
