9274b8068642b2c47c808a00a50004fad534ec93
Olayinka Adebisi <adebisiolayinka30@gmail.com>
2022-06-11 00:07:13 +0100

    Create new views for the blog-website

--- mysite/blog/urls.py
+++ mysite/blog/urls.py
@@ -4,5 +4,9 @@ from . import views
 urlpatterns = [
 path('about/', views.AboutView.as_view(), name='about'),
 path('', views.PostListView.as_view(), name='post-list'),
-path('post/<int:pk>,' views.PostDetailView.as_view(), name='post_detail')
+path('post/<int:pk>/' views.PostDetailView.as_view(), name='post_detail'),
+path('post/new/', views.CreatePostView.as_view(), name='post_new'),
+path('post/<int:pk>/edit/', views.PostUpdateView.as_view, name='post_edit'),
+path('post/<int:pk>/remove/', views.PostDeleteView.as_view(), name='post_remove'),
+path('drafts/', views.DraftListView.as_view(), name='post_draft_list')
 ]
