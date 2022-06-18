a3e20c74400338cddcb92e64b49c367c43a07e39
Olayinka Adebisi <adebisiolayinka30@gmail.com>
2022-06-18 22:03:30 +0100

    self sigh......

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -9,13 +9,13 @@ https://docs.djangoproject.com/en/4.0/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.0/ref/settings/
 """
-
+import os
 from pathlib import Path
 from decouple import config
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
-TEMPLATE_DIR = Path(BASE_DIR) / 'blog/templates/blog'
+TEMPLATE_DIR = os.path.join(BASE_DIR, 'blog/templates/blog')
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
